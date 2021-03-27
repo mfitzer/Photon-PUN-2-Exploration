3f0e38311e071da98def207ff045f07de1c7435d
Marty Fitzer <martyfitzer@gmail.com>
2021-03-27 13:15:53 -0500

    Adds XR Interaction Tookit
    - Imports Unity XR Interaction Toolkit
    - Creates XRUIInputConfigurer to manage configuration of Canvas and EventSystem based on whether XR is enabled (mainly for debugging purposes)

---
+++ Photon PUN 2 Exploration/Assets/Scripts/Editor/ReadOnlyFieldPropertyDrawer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomPropertyDrawer(typeof(ReadOnlyFieldAttribute))]
+public class ReadOnlyFieldPropertyDrawer : PropertyDrawer
+{
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        EditorGUI.BeginProperty(position, label, property);
+
+        GUI.enabled = false;
+        EditorGUI.PropertyField(position, property, label, true);
+        GUI.enabled = true;
+
+        EditorGUI.EndProperty();
+    }
+}
---
+++ Photon PUN 2 Exploration/Assets/Scripts/Interactions/XRUIInputConfigurer.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem.UI;
+using UnityEngine.UI;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit.UI;
+
+namespace mfitzer.Interactions
+{
+    [RequireComponent(typeof(Canvas))]
+    [RequireComponent(typeof(GraphicRaycaster))]
+    [RequireComponent(typeof(TrackedDeviceGraphicRaycaster))]
+    public class XRUIInputConfigurer : MonoBehaviour
+    {
+        #region Canvas Config
+
+        [SerializeField, ReadOnlyField]
+        private GraphicRaycaster graphicRaycaster;
+
+        [SerializeField, ReadOnlyField]
+        private TrackedDeviceGraphicRaycaster xrGraphicRaycaster;
+
+        #endregion Canvas Config
+
+        #region Event System Config
+
+        [SerializeField]
+        private EventSystem eventSystem;
+
+        [SerializeField, ReadOnlyField]
+        private InputSystemUIInputModule uiInputModule;
+
+        [SerializeField, ReadOnlyField]
+        private XRUIInputModule xrUIInputModule;
+
+        #endregion
+
+        private void OnValidate()
+        {
+            getReferences();
+        }
+
+        private void Start()
+        {
+            getReferences();
+            configure();
+        }
+
+        private void getReferences()
+        {
+            if (!graphicRaycaster)
+                graphicRaycaster = GetComponent<GraphicRaycaster>();
+            if (!xrGraphicRaycaster)
+                xrGraphicRaycaster = GetComponent<TrackedDeviceGraphicRaycaster>();
+
+            if (!eventSystem)
+            {
+                eventSystem = FindObjectOfType<EventSystem>();
+                if (!eventSystem)
+                    Debug.LogError("There is no EventSystem in the scene. An EventSystem is required for UI interactions to function properly.");
+            }
+            if (eventSystem && !uiInputModule)
+            {
+                uiInputModule = eventSystem.GetComponent<InputSystemUIInputModule>();
+                if (!uiInputModule)
+                    uiInputModule = eventSystem.gameObject.AddComponent<InputSystemUIInputModule>();
+            }
+            if (eventSystem && !xrUIInputModule)
+            {
+                xrUIInputModule = GetComponent<XRUIInputModule>();
+                if (!xrUIInputModule)
+                    xrUIInputModule = eventSystem.gameObject.AddComponent<XRUIInputModule>();
+            }
+        }
+
+        private void configure()
+        {
+            graphicRaycaster.enabled = !XRSettings.enabled;
+            xrGraphicRaycaster.enabled = XRSettings.enabled;
+
+            uiInputModule.enabled = !XRSettings.enabled;
+            xrUIInputModule.enabled = XRSettings.enabled;
+        }
+    }
+}
